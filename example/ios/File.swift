b48c98af1d87387fd86d27a3a2409c9298846d3a
еkalmanbetov <esen.kalmanbetov98@gmail.com>
2022-04-12 13:14:34 +0600

    chore: initial commit

---
+++ example/ios/File.swift
@@ -0,0 +1,6 @@
+//
+//  File.swift
+//  WifiControlExample
+//
+
+import Foundation
---
+++ ios/WifiControl.swift
@@ -0,0 +1,8 @@
+@objc(WifiControl)
+class WifiControl: NSObject {
+
+    @objc(multiply:withB:withResolver:withRejecter:)
+    func multiply(a: Float, b: Float, resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock) -> Void {
+        resolve(a*b)
+    }
+}
